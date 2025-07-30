2cb9c76025bf16fd1adff1f1714836403e59f453
NekoStream <akkuramola11@gmail.com>
2025-07-30 22:10:46 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-const animeContainer = document.getElementById('anime-container');
-animeList.forEach(anime => {
-  const card = document.createElement('div');
-  card.classList.add('anime-card');
-  card.innerHTML = `
-    <a href="${anime.link}" target="_blank">
-      <img src="${anime.image}" alt="${anime.title}">
-      <h3>${anime.title}</h3>
-    </a>
-  `;
-  animeContainer.appendChild(card);
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("NekoStream loaded!");
 });
